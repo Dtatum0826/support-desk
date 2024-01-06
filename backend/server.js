372037be129bdf25d41c2a194b88599720398a7d
Dustin Tatum <dtatum0826@gmail.com>
2024-01-06 13:16:54 -0600

    Database connected

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,18 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
 const PORT = process.env.PORT || 8000
+const colors = require('colors')
+const {errorHandler} = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
+
+// Connect to database
+connectDB()
 
 const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+
 
 app.get('/', (req,res) =>{
   res.status(200).json({message: "Welcom to support desk API"})
@@ -11,5 +21,7 @@ app.get('/', (req,res) =>{
 
 // Routes
 app.use('/api/users', require('./routes/userRoutes'))
+
+app.use(errorHandler)
 
 app.listen(PORT, ()=> console.log(` Server listening on port ${PORT}`))
