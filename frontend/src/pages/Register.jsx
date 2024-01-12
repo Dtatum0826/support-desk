cfc9508495a9fe5cf75e0a32954a15ff5e3c9faa
Dustin Tatum <dtatum0826@gmail.com>
2024-01-12 17:39:58 -0600

    Login functionality wired to Redux

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -5,6 +5,7 @@ import {toast} from 'react-toastify'
 import {FaUser} from'react-icons/fa'
 import { useSelector, useDispatch,  } from 'react-redux'
 import {register, reset} from '../features/auth/authSlice'
+import Spinner from '../components/Spinner'
 
 function Register() {
  const [formData, setFormData] = useState({
@@ -32,6 +33,8 @@ function Register() {
     }
     dispatch(reset())
   },[isError, isSuccess,user,message, navigate, dispatch])
+  
+  
   const onChange = (e) =>{
     setFormData((prevState) => ({
       ...prevState,
@@ -51,6 +54,10 @@ function Register() {
       }
       dispatch(register(userData))
     }
+  }
+
+  if(isLoading){
+    return <Spinner/>
   }
   return (
     <>
